d5bdbc6b94bd75010a222a3b59b875ac7acd98e9
a <a@gmail.com>
2023-01-07 17:58:06 -0800

    Add working history tool, need to generate graphs

--- snapshot-history.py
+++ snapshot-history.py
@@ -1,41 +1,48 @@
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
+
 # Select your transport with a defined url endpoint
 transport = AIOHTTPTransport(url="https://hub.snapshot.org/graphql")
 
 # Create a GraphQL client using the defined transport
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
-# Provide a GraphQL query
+# Historical voting results
 query = gql(
     """
 query Votes {
-  votes (
-    first: 100
-    skip: 100
+  votes(
+    first: 1000
+    skip: 0
     where: {
-      proposal: "QmPvbwguLfcVryzBRrbY4Pb9bCtxURagdv1XjhtFLf3wHj"
+      proposal: "0xd817c716ff271f5141cfe112e1fd7b652f961b4c965852eb44d8ed1ec3a5d3b5"
     }
     orderBy: "created",
     orderDirection: desc
   ) {
-    id
-    voter
-    created
-    proposal {
-      id
-    }
+    vp
     choice
-    space {
-      id
-    }
+    created
   }
 }
-
 """
 )
 
 # Execute the query on the transport
 result = client.execute(query)
-print(result)
+votes = result["votes"]
+history = [{'timestamp': 0, '1': 0, '2': 0, '3': 0}]  # array of dicts [timestamp, choice 1 count, choice 2 count, choice 3 count]
+for i in range(0, len(votes)):
+    prev = history[i]
+    choice = votes[i]['choice']
+    new = {}
+    if choice == 1:
+        new = {'timestamp': votes[i]['created'], '1': prev['1'] + votes[i]['vp'], '2': prev['2'], '3': prev['3']}
+    elif choice == 2:
+        new = {'timestamp': votes[i]['created'], '1': prev['1'], '2': prev['2'] + votes[i]['vp'], '3': prev['3']}
+    else:
+        new = {'timestamp': votes[i]['created'], '1': prev['1'], '2': prev['2'], '3': prev['3'] + votes[i]['vp']}
+    history.append(new)
+del history[0]
+print(history)
